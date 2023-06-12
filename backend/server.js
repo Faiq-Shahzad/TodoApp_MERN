4b7360e86a2eb0e4b457199ac3740ace714953e8
Faiq Shahzad <f03315558407@gmail.com>
2023-06-12 20:02:33 +0500

    Deployed on Docker

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,12 @@ const PORT = process.env.port || 3000;
 app.use(express.json());
 app.use(cors());
 
+const mongoURL = process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/TODO_App";
+
 
 //Connecting to MongoDB
 mongoose
-  .connect(process.env.MONGODB_URL)
+  .connect(mongoURL)
   .then(() => console.log("connected to MONGO DB"))
   .catch((err) => console.log(err));
 
